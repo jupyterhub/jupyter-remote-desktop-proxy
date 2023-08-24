a6e6897c6dca4d76789956a05c20af46eb876879
YuviPanda <yuvipanda@gmail.com>
2023-08-23 19:24:34 -0700

    Use :latest tag to build
    Otherwise, docker goes and pulls the base image from dockerhub,
    rather than using whatever is already in the local docker cache!

--- images/build.py
+++ images/build.py
@@ -153,7 +153,9 @@ def main():
     for image in to_build:
         base_image_spec = f"{args.image_prefix}{image}"
 
-        build(image, base_image_spec, build_args, args.platforms)
+        image_spec = f"{base_image_spec}:latest"
+
+        build(image, image_spec, build_args, args.platforms)
 
         tags = get_tags(image, base_image_spec)
 
