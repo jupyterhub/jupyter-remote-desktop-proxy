4adc96e1c8ca3cc1e71977859d9c9f3be9305cc0
YuviPanda <yuvipanda@gmail.com>
2023-09-26 19:33:49 -0700

    Install forked version of qgis-stac-plugin
    Necessary for s3 support

--- images/qgis/setup-qgis-plugins.bash
+++ images/qgis/setup-qgis-plugins.bash
@@ -6,9 +6,13 @@ set -euo pipefail
 # Tell qgis-plugin-manager where our qgis plugins are
 export QGIS_PLUGINPATH=/opt/conda/share/qgis/python/plugins
 
-# Initialize the qgis plugin manager
-qgis-plugin-manager init
-qgis-plugin-manager update
+# Install qgis-stac-plugin from our fork, to add support for reading from S3
+git clone https://github.com/slesaad/qgis-stac-plugin
+cd qgis-stac-plugin
+git checkout support_gdal_assets
+pip install --no-cache .
 
-# Install the STAC API Browser plugin: https://stac-utils.github.io/qgis-stac-plugin/
-qgis-plugin-manager install "STAC API Browser"
+python admin.py build
+cp -r build/qgis_stac/ ${QGIS_PLUGINPATH}/qgis_stac/
+cd ..
+rm -rf qgis-stac-plugin
