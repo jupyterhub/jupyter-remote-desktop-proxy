491d81434609fb797f001729fecb78a83e2cffe4
Erik Sundell <erik.i.sundell@gmail.com>
2024-03-12 00:33:57 +0100

    asdf

--- 1.sh
+++ 1.sh
@@ -19,6 +19,11 @@ timeout --preserve-status 1 nc -v localhost 5901 2>&1 | \
   grep --quiet RFB && echo "Passed outside test" || { echo "Failed outside test" && TEST_OK=false; }
 # echo "::endgroup::"
 
+echo "netstat inside container"
+docker exec $container_id netstat -na --tcp | grep -E "(:5901|:5902)"
+echo "netstat outside container"
+netstat -na --tcp | grep -E "(:5901|:5902)"
+
 # echo "::group::vncserver logs"
 # docker exec $container_id bash -c "cat ~/.vnc/*.log"
 # echo "::endgroup::"
