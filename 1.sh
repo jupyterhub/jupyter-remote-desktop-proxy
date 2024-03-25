91ab7ae87e69efed2014fb895a11c489b45584c4
Erik Sundell <erik@sundellopensource.se>
2024-03-25 23:23:50 +0100

    Get a TTY for tests

--- 1.sh
+++ 1.sh
@@ -1,24 +1,8 @@
 container_id=$(docker run -d -p 5901:5901 --security-opt seccomp=unconfined quay.io/consideratio/test:turbo vncserver -xstartup /opt/install/jupyter_remote_desktop_proxy/share/xstartup -verbose -fg -geometry 1680x1050 -SecurityTypes None -rfbport 5901)
-# container_id=$(docker run -d -p 5901:5901 -v $(pwd):/mnt/test quay.io/consideratio/test:turbo python /mnt/test/dummy-tcp-server.py)
-sleep 3
-
-# echo "::group::Installing netcat (inside container)"
-# docker exec --user root $container_id bash -c '
-#   apt update
-#   apt install -y netcat net-tools
-# '
-# echo "::endgroup::"
-
-docker exec $container_id bash -c 'timeout --preserve-status 1 nc -v localhost 5901' 2>&1 | \
-  grep --quiet RFB && echo "Passed inside test" || { echo "Failed inside test" && TEST_OK=false; }
+sleep 1
 
 timeout --preserve-status 1 nc -v localhost 5901 2>&1 | \
-  grep --quiet RFB && echo "Passed outside test" || { echo "Failed outside test" && TEST_OK=false; }
-
-echo "netstat inside container"
-docker exec $container_id netstat -na --tcp | grep -E "(:5901|:5902)"
-echo "netstat outside container"
-netstat -na --tcp | grep -E "(:5901|:5902)"
+  grep --quiet RFB && echo "Passed test" || { echo "Failed test" && TEST_OK=false; }
 
 docker stop $container_id > /dev/null
 if [ "$TEST_OK" == "false" ]; then
--- 2.sh
+++ 2.sh
@@ -1,25 +1,8 @@
 container_id=$(docker run -d -p 5901:5901 quay.io/consideratio/test:turbo websockify --verbose --log-file=/tmp/websockify.log --heartbeat=30 5901 -- vncserver -xstartup /opt/install/jupyter_remote_desktop_proxy/share/xstartup -verbose -fg -geometry 1680x1050 -SecurityTypes None -rfbport 5901)
-# container_id=$(docker run -d -p 5901:5901 -v $(pwd):/mnt/test quay.io/consideratio/test:turbo websockify --verbose --log-file=/tmp/websockify.log --heartbeat=30 5901 -- python /mnt/test/dummy-tcp-server.py)
-sleep 3
-
-# echo "::group::Installing websocat (inside container)"
-# docker exec --user root $container_id bash -c '
-#   wget -q https://github.com/vi/websocat/releases/download/v1.12.0/websocat.x86_64-unknown-linux-musl \
-#     -O /usr/local/bin/websocat
-#   chmod +x /usr/local/bin/websocat
-# '
-# echo "::endgroup::"
-
-docker exec -it $container_id bash -c 'websocat --binary --one-message --exit-on-eof "ws://localhost:5901/"' 2>&1 | \
-  grep --quiet RFB && echo "Passed inside test"  || { echo "Failed inside test" && TEST_OK=false; }
+sleep 1
 
 websocat --binary --one-message --exit-on-eof "ws://localhost:5901/" 2>&1 | \
-  grep --quiet RFB && echo "Passed outside test" || { echo "Failed outside test" && TEST_OK=false; }
-
-echo "netstat inside container"
-docker exec $container_id netstat -na --tcp | grep -E "(:5901|:5902)"
-echo "netstat outside container"
-netstat -na --tcp | grep -E "(:5901|:5902)"
+  grep --quiet RFB && echo "Passed test" || { echo "Failed test" && TEST_OK=false; }
 
 docker stop $container_id > /dev/null
 if [ "$TEST_OK" == "false" ]; then
