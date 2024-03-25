3edb33903d3bd41ab4e38de56b839b628d4be447
Erik Sundell <erik@sundellopensource.se>
2024-03-26 00:19:28 +0100

    don't pipefail

--- 1.sh
+++ 1.sh
@@ -1,4 +1,4 @@
-container_id=$(docker run -d -p 5901:5901 --security-opt seccomp=unconfined quay.io/consideratio/test:turbo vncserver -xstartup /opt/install/jupyter_remote_desktop_proxy/share/xstartup -verbose -fg -geometry 1680x1050 -SecurityTypes None -rfbport 5901)
+container_id=$(docker run -d -p 5901:5901 quay.io/consideratio/test:turbo vncserver -xstartup /opt/install/jupyter_remote_desktop_proxy/share/xstartup -verbose -fg -geometry 1680x1050 -SecurityTypes None -rfbport 5901)
 sleep 1
 
 timeout --preserve-status 1 nc -v localhost 5901 2>&1 | \
