24c33bab6942da59a66c7517f86e74fab405b370
YuviPanda <yuvipanda@gmail.com>
2024-06-13 08:53:23 -0700

    Bump to 2.0.1

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ with open("README.md") as f:
 setup(
     name="jupyter-remote-desktop-proxy",
     packages=find_packages(),
-    version='2.0.1.dev',
+    version='2.0.1',
     author="Jupyter Development Team",
     author_email="jupyter@googlegroups.com",
     classifiers=[
